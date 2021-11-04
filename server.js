8c4b91c40979ba1fee53a67dd742ba514668b583
Isuru Pathum Herath <isurupathumherath@gmail.com>
2021-11-05 00:51:31 +0530

    primary files and packages

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+// import routes
+// const postRoutes = require('./routes/post');
+
+// App
+const app = express();
+
+// Database
+mongoose
+    .connect(process.env.DATABASE, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("DB Connected"))
+    .catch((err) => console.log(err));
+
+// Middlewares
+app.use(cors());
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+// Route Middleware
+// app.use('/api', postRoutes);
+
+// Post
+const port = process.env.PORT || 8000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
