1be0c2d319e11479fd96759efd2509473f38d9b6
IT20206178 <77095664+IT20206178@users.noreply.github.com>
2021-11-16 23:12:16 +0530

    customer backend
    only create & read

---
+++ models/customer.js
@@ -0,0 +1,81 @@
+const mongoose = require('mongoose');
+const { ObjectId } = mongoose.Schema;
+
+const customerSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        trim: true,
+        max: 255,
+        required: true
+    },
+    middleName: {
+        type: String,
+        trim: true,
+        max: 255,
+        required: false
+    },
+    lastName: {
+        type: String,
+        trim: true,
+        max: 255,
+        required: true
+    },
+    address: {
+        type: String,
+        max: 255,
+        required: true
+    },
+    emailAddress: {
+        type: String,
+        max: 255,
+        required: true
+    },
+    mobileNumber: {
+        type: String,
+        max: 10,
+        required: true
+    },
+    nic: {
+        type: String,
+        trim: true,
+        max: 12,
+        required: true
+    },
+    dob: {
+        type: Date,
+        required: true
+    },
+    gender: {
+        type: String,
+        required: true
+    },
+    companyid: {
+        type: String,
+        trim: true,
+        max: 255,
+        required: true
+    },
+    balance: {
+        type: String,
+        max: 255,
+        required: true
+    },
+    addedAt: {
+        type: Date,
+        required: true
+    },
+    updatedAt: {
+        type: Date,
+        required: false
+    },
+    addedBy: {
+        type: String,
+        required: true
+    },
+    updatedBy: {
+        type: String,
+        required: false
+    },
+}, { timestamps: true })
+
+module.exports = mongoose.model('Customer', customerSchema);
