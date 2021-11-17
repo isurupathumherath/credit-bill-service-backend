cbdf9b7bb3e631efc5f972545479a3da5a7327af
Isuru Pathum Herath <isurupathumherath@gmail.com>
2021-11-17 11:31:01 +0530

    JWT TOKEN Added nd verified for all Endponts

---
+++ controllers/user.js
@@ -0,0 +1,123 @@
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+const expressJwt = require('express-jwt');
+require('dotenv').config();
+
+//Add New User
+exports.create = (req, res) => {
+    const { firstName, middleName, lastName, address, emailAddress, mobileNumber, nic, dob, gender, username, password, role, addedBy } = req.body;
+
+    switch (true) {
+        case !firstName:
+            return res.status(404).json({
+                error: 'First Name is Required'
+            });
+        case !lastName:
+            return res.status(404).json({
+                error: 'Last Name is Required'
+            });
+        case !address:
+            return res.status(404).json({
+                error: 'Address is Required'
+            });
+        case !emailAddress:
+            return res.status(404).json({
+                error: 'Email is Required'
+            });
+        case !mobileNumber:
+            return res.status(404).json({
+                error: 'Mobile Number is Required'
+            });
+        case !nic:
+            return res.status(404).json({
+                error: 'NIC is Required'
+            });
+        case !dob:
+            return res.status(404).json({
+                error: 'Date of Birth is Required'
+            });
+        case !gender:
+            return res.status(404).json({
+                error: 'Gender is Required'
+            });
+        case !username:
+            return res.status(404).json({
+                error: 'Username is Required'
+            });
+        case !password:
+            return res.status(404).json({
+                error: 'Password is Required'
+            });
+        case !role:
+            return res.status(404).json({
+                error: 'Role is Required'
+            });
+        case !addedBy:
+            return res.status(404).json({
+                error: 'Added Person is Required'
+            });
+    }
+
+    User.create({ firstName, middleName, lastName, address, emailAddress, mobileNumber, nic, dob, gender, username, password, role, addedBy }, (err, user) => {
+        if (err) {
+            console.log(err)
+            res.status(404).json({
+                error: 'Record is not added'
+            })
+        }
+        res.json(user);
+    })
+}
+
+//Display All Users
+exports.read = (req, res) => {
+    User.find({})
+        // .limit(10)
+        .sort({ createdAt: -1 })
+        .exec((err, users) => {
+            if (err) console.log(err);
+            res.json(users);
+        });
+};
+
+//Display One User by Id
+exports.readById = (req, res) => {
+    console.log(req.params.id)
+    User.findById(req.params.id)
+        .exec((err, user) => {
+            if (err) console.log(err);
+            res.json(user);
+        });
+};
+
+//Login 
+exports.login = (req, res) => {
+    const { username, password } = req.body;
+    User.findOne({ username: username, password: password })
+        .exec((err, user) => {
+            if (err) {
+                if (err.keyPattern.username == 1) {
+                    res.status(400).json({
+                        error: 'Username is already in use'
+                    });
+                }
+                else {
+                    res.status(400).json({
+                        error: 'Internal Server Error! Try Again!'
+                    });
+                }
+            } else if (user != null) {
+                const token = jwt.sign({ username }, process.env.JWT_SECRET, { expiresIn: '1d' })
+                res.json({ token, user });
+            } else {
+                res.json(user);
+            }
+
+
+        });
+};
+
+exports.requireSignin = expressJwt({
+    secret: process.env.JWT_SECRET,
+    algorithms: ['sha1', 'RS256', 'HS256']
+});
---
+++ routes/user.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+//import controller function names
+const { create, read, readById, requireSignin } = require('../controllers/user');
+
+router.post('/add', requireSignin, create);
+
+router.get('/users', read);
+router.get('/userById/:id', requireSignin, readById);
+
+// router.get('/secret', requireSignin, (req, res) => {
+//     res.json({
+//         data: req.user.username
+//     })
+// })
+
+module.exports = router;
